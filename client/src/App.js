462029fdc4e214859b63f45bbbb9d14ab27a2c3e
sidchopp <siddchopp@gmail.com>
2024-03-18 13:34:13 -0600

    CORS headers added, URI updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,4 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 //Components
 import BookList from "./components/BookList";
@@ -11,10 +6,11 @@ import AddBook from "./components/AddBook";
 
 //Apolo Client set up
 const client = new ApolloClient({
-  uri: "https://books-merng-server.herokuapp.com/graphql",
+  uri: "https://books-server-vercel-six.vercel.app/graphql",
   cache: new InMemoryCache(),
 });
 
+console.log("client", client);
 function App() {
   return (
     <ApolloProvider client={client}>
